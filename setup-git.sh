68d314178332661cf087f32a2d4a08ee62ae84b8
sohammondal <soham.pf.mondal@gmail.com>
2020-05-30 12:50:06 +0530

    multi git profile setup script

---
+++ setup-git.sh
@@ -0,0 +1,38 @@
+# Source - https://stackoverflow.com/questions/22697688/how-to-cat-eof-a-file-containing-code/22698106
+# Source - https://bash.cyberciti.biz/guide/If..else..fi
+
+cat <<EOF > ~/.gitconfig-personal
+[user]
+name = sohammondal
+email = soham.pf.mondal@gmail.com
+EOF
+
+
+
+cat <<EOF > ~/.gitconfig-work
+[user]
+name = 
+email = 
+EOF
+
+cat <<EOF > ~/.gitconfig-test
+[includeIf "gitdir:~/Work/"]
+  path = .gitconfig-work
+
+[includeIf "gitdir:~/Personal/"]
+  path = .gitconfig-personal
+EOF
+
+if mkdir ~/Work
+then
+    echo 'Office Workspace created.'
+else
+    echo 'Office Workspace exists.'
+fi
+
+if mkdir ~/Personal
+then
+    echo 'Personal Workspace created.'
+else
+    echo 'Personal Workspace exists.'
+fi
