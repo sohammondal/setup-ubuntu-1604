695ecb67041caadc59bbc8c929aaf192858efd1d
sohammondal <soham.pf.mondal@gmail.com>
2020-05-30 12:52:47 +0530

    add 1 script to execute all

---
+++ start.sh
@@ -0,0 +1,23 @@
+bash basics.sh
+
+bash docker.sh
+
+bash chrome.sh
+
+bash vlc.sh
+
+bash spotify.sh
+
+bash update-git.sh
+
+bash setup-git.sh
+
+bash vs-code.sh
+
+bash nodejs.sh
+
+bash snapd.sh
+
+bash postman.sh
+
+bash arc-theme.sh
